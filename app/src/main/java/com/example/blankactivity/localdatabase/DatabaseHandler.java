fe5b8ae7a1a0812125e086423daeeb72504a34e5
diwashadhikari <diwashadhikari@bottle.com.np>
2023-05-18 08:19:59 +0545

    Complete : local database implementation

--- app/src/main/java/com/example/blankactivity/localdatabase/DatabaseHandler.java
+++ app/src/main/java/com/example/blankactivity/localdatabase/DatabaseHandler.java
@@ -2,6 +2,7 @@ package com.example.blankactivity.localdatabase;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -13,14 +14,15 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        onCreate(db);
 
-        //3rd argument to be passed is CursorFactory instance
     }
 
     // Creating Tables
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String create_table = "CREATE TABLE " + DATABASE_NAME + "(id INTEGER PRIMARY KEY,name TEXT, address TEXT)";
+        String create_table = "CREATE TABLE " + "MyTable" + "(ID INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, address TEXT)";
         db.execSQL(create_table);
     }
 
@@ -33,16 +35,66 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
 
 
-    void addData(String name, String address) {
+    void addData(Integer id,String name, String address) {
         SQLiteDatabase db = this.getWritableDatabase();
 
         ContentValues values = new ContentValues();
+//        values.put("id", id); // Contact Name
         values.put("name", name); // Contact Name
         values.put("address", address); // Contact Phone
 
-        db.insert(DATABASE_NAME, null, values);
+        db.insert("MyTable", null, values);
         db.close(); // Closing database connection
     }
 
+    public Cursor getAllData() {
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + "MyTable";
 
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+
+                System.out.println(cursor.getString(0));
+                System.out.println(cursor.getString(1));
+
+            } while (cursor.moveToNext());
+        }
+
+        // return contact list
+        return cursor;
+    }
+
+    Cursor getContact(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query("MyTable", new String[] { "id",
+                        "name", "address" }, "id" + "=?",
+                new String[] { String.valueOf(id) }, null, null, null, null);
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        System.out.println(cursor.getString(0));
+        System.out.println(cursor.getString(1));
+        System.out.println(cursor.getString(2));
+        return cursor;
+    }
+
+    public int update() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", "NewName");
+        contentValues.put("address", "NewAddress");
+        int i = db.update("MyTable", contentValues, "id" + " = " + "1", null);
+        System.out.println(i);
+        return i;
+    }
+
+    public void delete() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete("MyTable", "id" + "=" + "2", null);
+    }
 }
--- app/src/main/java/com/example/blankactivity/localdatabase/LocalDatabase.java
+++ app/src/main/java/com/example/blankactivity/localdatabase/LocalDatabase.java
@@ -1,8 +1,8 @@
 package com.example.blankactivity.localdatabase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.blankactivity.R;
 
@@ -13,7 +13,22 @@ public class LocalDatabase extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_database);
 
-        DatabaseHandler handler =new  DatabaseHandler(this);
+        DatabaseHandler handler = new DatabaseHandler(this);
+
+        handler.addData(1,"Diwash", "BKT");
+        handler.addData(2,"Diwash1", "BKT1");
+        handler.addData(3,"Diwash2", "BKT2");
+        handler.addData(4,"Diwash3", "BKT3");
+
+        handler.getAllData();
+
+        handler.update();
+
+        handler.getAllData();
+        handler.delete();
+        handler.getAllData();
+
+        handler.getContact(3);
 
     }
 
